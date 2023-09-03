f85345b9f2d54b64d3e4ce6b0ae8bc0e77a0080c
Przemyslaw1986 <99801249+Przemyslaw1986@users.noreply.github.com>
2023-09-03 12:38:44 +0200

    some ESLINT shit

---
+++ 1 teoria/027 - ESLint i Prettier/main.js
@@ -0,0 +1,22 @@
+// metoda .AT
+const array = [111, 222, 333, 444];
+// array[array.length - 2];
+// array.at(-2);
+// Zatem - do obsługi tablicy, by wskazać konktetny Element, zamiast używać array length, możemy użyć metody .at z indeksem w nawiasie - jeśli index będzie z minusem, będzie liczył od końca.
+
+// ------Konfiguracja projektu i ESLint-----
+
+// w terminalu:
+
+// node -v   - czy masz node
+// npm -v     - (npm = node package manager)
+// nmp init   (npm init -y ( y = yes))
+
+const a = "sasasa";
+let b = 354;
+
+// npm install --save-dev eslint
+// npm init @eslint/config
+
+let variable = "boso";
+let v1 = `olo`;
