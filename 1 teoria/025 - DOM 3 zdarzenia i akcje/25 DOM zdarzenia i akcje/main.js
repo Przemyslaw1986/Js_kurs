80ae04d8ab6d65f173b00a9fd046617624c8c8e1
Przemyslaw1986 <99801249+Przemyslaw1986@users.noreply.github.com>
2023-09-03 12:34:52 +0200

    ESLint and prettier is On

--- 1 teoria/025 - DOM 3 zdarzenia i akcje/25 DOM zdarzenia i akcje/main.js
+++ 1 teoria/025 - DOM 3 zdarzenia i akcje/25 DOM zdarzenia i akcje/main.js
@@ -4,14 +4,13 @@
 
 //PRZYKŁAD
 // - jako pierwszy argument podajemy nazwę zdarzenie w tym przypadku podwójne kliknięcie czyli "dblclick".
-// jako drugi argument podajemy funkcję (callback), która ma być wywołana w moemencie wystapienie zdarzenia (za każdym razem gdy wystąpi) 
-window.addEventListener("dblclick", function() {
- console.log("podwójne kliknięcie");
+// jako drugi argument podajemy funkcję (callback), która ma być wywołana w moemencie wystapienie zdarzenia (za każdym razem gdy wystąpi)
+window.addEventListener("dblclick", function () {
+  console.log("podwójne kliknięcie");
 });
 
 // PRZYKŁAD - możemy podać funkcję strzałkową
 // document.body.addEventListener("click", () => console.log("kliknięcie"))
-
 
 // PRZYKŁAD - mozemy też podać nazwę funkcji, która została stworzona poza metodą addEventListener
 // const showScroll = () => {
@@ -19,13 +18,12 @@ window.addEventListener("dblclick", function() {
 // }
 
 function showScroll() {
- console.log("skrol");
+  console.log("skrol");
 }
 
 window.addEventListener("scroll", showScroll);
 
-
-
+console.log("test eslinta");
 
 // PRZYKŁAD - metodę możemy przypisać bezposrednio do pobieranego elementu
 // document.querySelector('h1').addEventListener('mousemove', function() {
@@ -33,37 +31,31 @@ window.addEventListener("scroll", showScroll);
 //  console.log("ruch myszką po h1");
 // })
 
-
-
 // PRZYKŁAD
-const h1 = document.querySelector('h1');
+const h1 = document.querySelector("h1");
 
-const addText = function() {
- this.textContent += " :) "; //dodanie znaków (stringa) do istniejacej zawartości tekstowej
- h1.classList.toggle("red"); //dodanie/zabranie klasy z elementu (dodaje jak nie ma, zabiera jak jest)
-}
+const addText = function () {
+  this.textContent += " :) "; //dodanie znaków (stringa) do istniejacej zawartości tekstowej
+  h1.classList.toggle("red"); //dodanie/zabranie klasy z elementu (dodaje jak nie ma, zabiera jak jest)
+};
 
-h1.addEventListener('click', addText);
-
+h1.addEventListener("click", addText);
 
 // PRZYKŁAD - nastawienie nasłuchiwania, czyli uruchomienie metody addEventListener na każdym elemencie. Pamietajmy że nie można beżposrednio na pobranym obiekcie, tylko na każdym jego elemencie osobno
 
-const addClassRed = function() {
- console.log("klik li");
-}
+const addClassRed = function () {
+  console.log("klik li");
+};
 
 const items = document.querySelectorAll("li");
 // zapis items.addEventListener() nie zadziała, bo nie możemy ustawić nasłuchiwanie na tablicy tylko na pobranych elementach. Jak to zrobić, widać ponizej (można też użyć pętli)
 
 //Metoda forEach sprawia, ze podana jako argument funkcja, jest wykonywana na każdym elemencie tablicy (na każdym li) Przy czym przekazujemy do środka ten element nadając mu nazwę (w tym wypadku item) Przy wywołaniu funkcji na kzdym elemencie w tym item będzie znajdowało się kolejne il (czyli kolejna pozycja z tablicy)
-items.forEach((item) => item.addEventListener("click", addClassRed))
-
+items.forEach((item) => item.addEventListener("click", addClassRed));
 
 // TWORZENIE I DODAWANIE ELEMENTÓW DO STRONY
 
-const divElement = document.createElement('div');
+const divElement = document.createElement("div");
 divElement.textContent = "Ostatni DIV";
 divElement.style.backgroundColor = "red";
 document.body.appendChild(divElement);
-
-
